280be3933591851d44a04092acdcba45d4ab6070
Muhammad Ellbendl Satria <69744381+DecX-x@users.noreply.github.com>
2025-04-29 12:05:04 +0700

    Update model version to 'qwen-turbo-latest' and fix formatting in llm.ts

--- lib/llm.ts
+++ lib/llm.ts
@@ -8,13 +8,14 @@ dotenvConfig();
 const llm_key = process.env.MODELSTUDIO_API_KEY;
 
 const llm = new ChatOpenAI({
-    model: "qwen-turbo",
+    model: "qwen-turbo-latest",
     apiKey: llm_key,
     temperature: 0.7,
     configuration: {
-        baseURL: "https://dashscope-intl.aliyuncs.com/compatible-mode/v1"
+        baseURL: "https://dashscope-intl.aliyuncs.com/compatible-mode/v1",
+        
     },
-    streaming: true
+    streaming: true,
 });
 
 // system message untuk memberikan konteks pada model
